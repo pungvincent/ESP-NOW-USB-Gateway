6750d0622c9469d2a29a09371fd4f48fe3873948
Vincent PUNG <vincent.pung9@gmail.com>
2025-04-19 16:11:05 +0200

    HA Dimmable light config

--- Home Assistant Config/send_serial_cmd.py
+++ Home Assistant Config/send_serial_cmd.py
@@ -5,11 +5,13 @@ import sys
 
 name = sys.argv[1] if len(sys.argv) > 1 else "unknown"
 cmd = sys.argv[2] if len(sys.argv) > 2 else "UNKNOWN"
+luminosity = sys.argv[3] if len(sys.argv) > 3 else "0"
 
 msg = {
     "type": "switch",
     "name": name,
-    "cmd": cmd.upper()
+    "cmd": cmd.upper(),
+    "luminosity": luminosity
 }
 
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
